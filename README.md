72a30dfb5f2f5799e0ad668beca38a1a819134a6
blox-dev <bloxorz51gaming@gmail.com>
2023-03-04 18:14:21 +0100

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# EuroVault
+
+Problem description:
+- Create a visualization tool for someone that plans to move in an EU country to help them make the best choice, according to their priorities.
+
+Used databases:
+- [Eurostat](https://ec.europa.eu/eurostat/data/database)
+
+Used frameworks:
+- [Danfo.js](https://danfo.jsdata.org/)
+- [Leaflet](https://leafletjs.com/SlavaUkraini/)
+
+
+Installation details:
+- Install nodejs >= 14.0
+- `npm install`
+- `npm start`
---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const fs = require('fs');
+const { format } = require('path');
+const app = express();
+const favicon = require('serve-favicon');
+
+app.set('view engine', 'ejs');
+
+// favicon
+app.use(favicon(__dirname + "/public/favicon.ico"));
+
+// static folders
+app.use('/data', express.static('data'));
+app.use('/public', express.static('public'));
+
+// routes
+app.get('/', async function (_req, res) {
+    res.render('pages/index', {});
+});
+
+// EUROSTAT_URL = "https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/nama_10_gdp?format=JSON&lang=EN&time=2019"
+
+BASE_URL = "https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/";
+SOME_URL = BASE_URL + "nama_10_gdp?format=JSON&lang=EN&na_item=B1GQ&unit=CLV_I15&sinceTimePeriod=2000";
+OTHER_URL = BASE_URL + "SDG_08_10?format=JSON&lang=EN";
+
+// Relevant fields codes
+iscedf13 = ["F01", "F02", "F03", "F04", "F05", "F0511", "F0512", "F052", "F053", "F054", "F06", "F07", "F071",
+"F072", "F073", "F08", "F09", "F091", "F0911", "F092", "F0921", "F0922", "F0923", "F10", "F1011", "F1012",
+"F1013", "F1014", "F1015", "F104"];
+
+// Relevant countries codes
+// "TR": "Türkiye" - maybe?
+geo = ["BE","BG","CZ","DK","DE","EE","IE","EL","ES","FR","HR","IT","CY","LV","LT","LU","HU","MT","NL","AT","PL",
+"PT","RO","SI","SK","FI","SE","IS","LI","NO","CH","UK","ME","MK","AL","RS","TR","BA","XK"];
+
+sinceTimePeriod = 2000;
+formatt = 'JSON';
+langg = 'EN';
+
+app.get('/update', async function(req, res) {
+    fs.readFile('./data/pull_metadata.json', (err, data) => {
+        if (err) {
+            res.status(404).send(err.message);
+        }
+        data = JSON.parse(data);
+        Object.entries(data).forEach(([key, val]) => {
+            if (val!=='') {
+                var url = BASE_URL;
+                url += val;
+                url += "&geo=" + geo.join("&geo=");
+                url += "&format=" + formatt;
+                url += "&lang=" + langg;
+                url += "&sinceTimePeriod=" + sinceTimePeriod;
+                console.log(key + ":\n--------------------------\n" + url + '\n');
+            }
+        });
+        res.send(data);
+    });
+});
+
+app.listen(3000);
